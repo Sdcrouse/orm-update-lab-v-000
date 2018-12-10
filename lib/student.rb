80698353619f2b81cc13edf92a133c6633261e38
Steven Crouse <sdcrouse91@gmail.com>
2018-12-10 19:16:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,5 @@
 require_relative "../config/environment.rb"
-
+require 'pry'
 class Student
 
   # Remember, you can access your database connection anywhere in this class
@@ -31,11 +31,23 @@ class Student
   end
   
   def save 
+<<<<<<< HEAD
     sql = "INSERT INTO students (name, grade) VALUES (?, ?)" 
     
     DB[:conn].execute(sql, self.name, self.grade)
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+=======
+    if self.id 
+      update
+    else 
+      sql = "INSERT INTO students (name, grade) VALUES (?, ?)" 
+      
+      DB[:conn].execute(sql, self.name, self.grade)
+      
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+>>>>>>> ac508d170d378f7bf63540cd406677b0e03677d4
   end
   
   def self.create(name, grade)
@@ -45,4 +57,28 @@ class Student
   def self.new_from_db(row)
     self.new(row[0], row[1], row[2])
   end
+<<<<<<< HEAD
+=======
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+      ORDER BY id 
+      LIMIT 1
+    SQL
+    
+    self.new_from_db( DB[:conn].execute(sql, name).first )
+  end
+  
+  def update 
+    sql = <<-SQL 
+      UPDATE students 
+      SET name = ?, grade = ?
+      WHERE id = ?
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+>>>>>>> ac508d170d378f7bf63540cd406677b0e03677d4
 end
